d0bb68ead3ea28f0917fe2783cf544c1319b7a10
virgesmith <a.p.smith@leeds.ac.uk>
2025-12-04 13:50:07 +0000

    neworder->xenoform

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Road-testing my new [itrx](https://pypi.org/project/itrx/) package: all solutions implemented using `itrx`
 unless entirely irrelevant.
+
+Also with a guest appearance from my other new package, [xenoform](https://pypi.org/project/itrx/), for some speedy goodness.
--- day4.py
+++ day4.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 import numpy as np
+import numpy.typing as npt
 from itrx import Itr
-from neworder.domain import StateGrid  # not cheating cos I wrote this package!
+from xenoform import compile
 
 test_data = """..@@.@@@@.
 @@@.@.@.@@
@@ -20,22 +21,53 @@ with Path("./day4.txt").open() as fd:
     data = fd.read()
 
 
+@compile(extra_compile_args=["-fopenmp"], extra_link_args=["-fopenmp"])
+def neighbours_with(grid: npt.NDArray[np.int32], *, value: int) -> npt.NDArray[np.int32]:  # type: ignore[empty-body]
+    """
+    py::buffer_info buf = grid.request();
+    if (buf.ndim != 2)
+        throw std::runtime_error("Input array must be 2D");
+
+    size_t m = buf.shape[0];
+    size_t n = buf.shape[1];
+
+    py::array_t<int> result({m, n});
+    auto r = result.mutable_unchecked<2>();
+    auto p = grid.unchecked<2>();
+
+    auto getval = [=](size_t i, size_t j) -> int {
+        return (i < 0 || i >= m || j < 0 || j >= n) ? 0 : (p(i, j) == value);
+    };
+
+    #pragma omp parallel for schedule(static)
+    for (size_t i = 0; i < m; ++i) {
+        for (size_t j = 0; j < n; ++j) {
+            r(i, j) = getval(i - 1, j - 1) + getval(i - 1, j) + getval(i - 1, j + 1)
+                    + getval(i    , j - 1)                    + getval(i    , j + 1)
+                    + getval(i + 1, j - 1) + getval(i + 1, j) + getval(i + 1, j + 1);
+        }
+    }
+
+    return result;
+    """
+
+
 def part1(data: str) -> int:
-    grid = StateGrid(np.array(Itr(data.split("\n")).map(lambda row: [{".": 0.0, "@": 1.0}[c] for c in row]).collect()))
-    n = grid.count_neighbours(lambda x: x > 0)
-    return np.logical_and(n < 4, grid.state == 1.0).sum()
+    grid = np.array(Itr(data.split("\n")).map(lambda row: [{".": 0, "@": 1}[c] for c in row]).collect())
+    n = neighbours_with(grid, value=1)
+    return np.logical_and(n < 4, grid == 1).sum()
 
 
 def part2(data: str) -> int:
-    grid = StateGrid(np.array(Itr(data.split("\n")).map(lambda row: [{".": 0.0, "@": 1.0}[c] for c in row]).collect()))
-    initial_count = grid.state.sum()
+    grid = np.array(Itr(data.split("\n")).map(lambda row: [{".": 0.0, "@": 1.0}[c] for c in row]).collect())
+    initial_count = grid.sum()
     while True:
-        n = grid.count_neighbours(lambda x: x > 0)
-        to_remove = np.logical_and(n < 4, grid.state == 1.0)
+        n = neighbours_with(grid, value=1)
+        to_remove = np.logical_and(n < 4, grid == 1)
         if to_remove.sum() == 0:
             break
-        grid.state -= to_remove
-    return int(initial_count - grid.state.sum())
+        grid -= to_remove
+    return int(initial_count - grid.sum())
 
 
 if __name__ == "__main__":
